5a64bc07144799f1c1d28cae9d8a29fdd48e9909
shaden-s <74512014+shaden-s@users.noreply.github.com>
2023-11-04 11:33:29 +0000

    First Prototype

---
+++ Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float sightRange, attackRange;
+    
+
+}
---
+++ Scripts/Enemies/FemaleSoldier.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+namespace cowsins { 
+public class FemaleSoldier : EnemyHealth
+{
+    public float speed = 1.5f;
+
+    public NavMeshAgent agent;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patroling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public int damage;
+    public float attackSpeed = 20f;
+
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void Update()
+    {   
+        //Handle UI 
+        if (healthSlider != null) healthSlider.value = Mathf.Lerp(healthSlider.value, health,Time.deltaTime * 6);
+        if (shieldSlider != null) shieldSlider.value = Mathf.Lerp(shieldSlider.value, shield, Time.deltaTime * 4); 
+
+        // Manage health
+        if (health <= 0) 
+        {
+            Die();
+        }
+        else
+        {
+            playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+            playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+            if (!playerInSightRange && !playerInAttackRange) Patroling();
+            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+            if (playerInAttackRange && playerInSightRange) AttackPlayer();
+            
+        }
+
+        //AI
+        //if (health > 0){
+            //RotateToTarget();
+            //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //}
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            GetComponent<Animator>().Play("Soldier_F_Attack_Machete");
+            player.GetComponent<PlayerStats>().Damage(damage);
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    public override void Die()
+    {   
+        agent.SetDestination(transform.position);
+        events.OnDeath.Invoke();
+        speed = 0;
+
+        if(shieldSlider != null)shieldSlider.gameObject.SetActive(false);
+        if (healthSlider != null) healthSlider.gameObject.SetActive(false);
+
+        //UIEvents.onEnemyKilled.Invoke(name); 
+
+        if (transform.GetComponent<CompassElement>() != null) transform.GetComponent<CompassElement>().Remove(); 
+
+        GetComponent<Animator>().Play("Soldier_F_Death");
+        Destroy(this.gameObject, 3f);
+    }
+
+    public void RotateToTarget()
+    {
+        //transform.LookAt(player.transform);
+        Vector3 direction = player.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+    }
+
+
+
+}
+}
